c31de9c80a9a0fdeaa16c6e85f48495285ffe6ab
David <dhutto_11@live.com>
2020-08-11 10:41:54 -0500

    :tada: First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    urlSrc: 'https://www.google.com/'
+  }
+  
+  handleLink(src) {
+    this.setState({urlSrc: src})
+  }
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-iFrame">
+        <iframe src={this.state.urlSrc} height="800" width="1200"></iframe>
+      </div>
+      <ul className="App-ul">
+        <li className="App-li" onClick={() => this.handleLink('https://www.linkedin.com/feed/')}>Camera 1</li>
+        <li className="App-li" onClick={() => this.handleLink('Camera 2 IP')}>Camera 2</li>
+        <li className="App-li" onClick={() => this.handleLink('Camera 3 IP')}>Camera 3</li>
+        <li className="App-li" onClick={() => this.handleLink('Camera 4 IP')}>Camera 4</li>
+        <li className="App-li" onClick={() => this.handleLink('Camera 5 IP')}>Camera 5</li>
+        <li className="App-li" onClick={() => this.handleLink('Camera 6 IP')}>Camera 6</li>
+      </ul>
+      
+     {/* <h1> this is the src: {this.state.urlSrc}</h1>  */}
     </div>
   );
+}
 }
 
 export default App;
